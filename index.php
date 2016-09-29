253f0db2583cf56fd4f8e2a477dd6f5c2fbcd821
isudzumi <isudzumi@gmail.com>
2016-09-30 02:06:41 +0900

    update view

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 $roomAry = [1112, 1111, 1110];
+$deskline = [			//1列の長さ
+	1112 => [5, 7, 7],
+	1111 => [5, 7, 6],
+	1110 => [5, 7, 7],
+];
 
 $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 $db['dbname'] = ltrim($db['path'], '/');
@@ -52,6 +57,35 @@ function h($var)
 		<div class='pc'>教員PC</div>
 
 			<div class='desk-area'>
+			<?php
+				$fstL = $deskline[$room][0];
+				$secL = $deskline[$room][1];
+				$thdL = $deskline[$room][2];
+			?>
+			<?php for($i = 1; $i<=($fstL+$secL+$thdL)*2; $i++):?>
+				<?php if($i == 1 || $i == 1+($fstL*2) || $i == 1+($fstL+$secL)*2):					//the number of the upper left desk ?>
+
+				<div class='desk-block desk-right'>
+					<div class='column right'>
+				<?php endif; ?>
+
+				<?php if($i == 1+$fstL || $i == 1+($fstL*2)+$secL || $i == 1+(($fstL+$secL)*2)+$thdL):	 		//the number of the upper right desk ?>
+
+					</div>
+					<div class='column left'>
+
+				<?php endif; ?>
+				<?php $stat = (h($result[$i-1]['status']) == 0) ? "" : 'style="background-color: yellow"' ; ?>
+
+							<div class="cell" ><?=$i?></a></div>
+
+				<?php if($i == $fstL*2 || $i == ($fstL+$secL)*2 || $i == ($fstL+$secL+$thdL)*2):			//the number of the lower left desk ?>
+
+					</div>
+				</div>
+
+				<?php endif; ?>
+			<?php endfor; ?>
 			</div>
 
 			<?php $dr = h($room) == 1110 ? "door-left" : "door-right" ?>
@@ -63,89 +97,6 @@ function h($var)
 		</div>
 
 	<?php endforeach; ?>
-
-	<div id="1112" class="room">
-	<div class="board">スクリーン</div>
-	<div class="pc" id="pc-1112">教員PC</div>
-		<div class="desk-area">
-			<?php
-			for($i=1; $i<=38; $i++){
-				if($i == 1 || $i == 11 || $i == 25){	//the number of the upper left desk
-					echo "<div class='desk-block desk-right'>\n", "\t<div class='column right'>\n";
-				}
-				if($i == 6 || $i == 18 || $i == 32) {	//the number of the upper right desk
-					echo "\t</div>\n", "\t<div class='column left'>\n";
-				}
-
-				$stat = (h($result[$i-1]['status']) == 0) ? "" : ' style="background-color: yellow"' ;
-					echo "\t\t<div class='cell'".$stat.">".$i."</a></div>\n";
-
-				if($i == 10 || $i == 24 || $i == 38){	//the number of the lower left desk
-					echo "\t</div>\n", "</div>\n";
-				}
-			}
-			?>
-		</div>
-		<div class="door-area door-area-right">
-			<div class="door wall door-right">前方ドア</div>
-			<div class="door door-right">後方ドア(※締切)</div>
-		</div>
-	</div>
-	<div id="1111" class="room">
-	<div class="board">スクリーン</div>
-	<div class = "pc" id="pc-1111">教員PC</div>
-		<div class="desk-area">
-			<?php
-			for($i=1; $i<=36; $i++){
-				if($i == 1 || $i == 11){
-					echo "<div class='desk-block desk-right'>\n", "\t<div class='column right'>\n";
-				} elseif ($i == 25){
-					echo "<div class='desk-block desk-right' id='desk-back'>\n", "\t<div class='column right'>\n";
-				}
-				if($i == 6 || $i == 18 || $i == 31){
-					echo "\t</div>\n", "\t<div class='column left'>\n";
-				}
-
-					echo "\t\t<div class='cell'>".$i."</div>\n";
-
-				if($i == 10 || $i == 24 || $i == 36){
-					echo "\t</div>\n", "</div>\n";
-				}
-			}
-			?>
-		</div>
-		<div class="door-area door-area-right">
-			<div class="door wall door-right">前方ドア(※締切)</div>
-			<div class="door door-right">後方ドア</div>
-		</div>
-	</div>
-
-	<div id="1110" class="room">
-	<div class="board">スクリーン</div>
-	<div class="pc" id="pc-1110">教員PC</div>
-		<div class="door-area door-area-left">
-			<div class="door wall door-left">前方ドア</div>
-			<div class="door door-left">後方ドア(※締切)</div>
-		</div>
-		<div class="desk-area door-right">
-			<?php
-			for($i=1; $i<=38; $i++){
-				if($i == 1 || $i == 11 || $i == 25){
-					echo "<div class='desk-block desk-left'>\n", "\t<div class='column left'>\n";
-				}
-				if($i == 6 || $i == 18 || $i == 32){
-					echo "\t</div>\n", "\t<div class='column right'>\n";
-				}
-
-					echo "\t\t<div class='cell'>".$i."</div>\n";
-
-				if($i == 10 || $i == 24 || $i == 38){
-					echo "\t</div>\n", "</div>\n";
-				}
-			}
-			?>
-		</div>
-	</div>
 	<div id="modal" class="is-hide">
 		<div id="modal-comment"></div>
 		<button type="submit" id="submit">OK</button> <button id="modal-close">Cancel</button> 
