36c7e244cc534d9a83aebb5bc49d5d0e740d8346
isudzumi <isudzumi@gmail.com>
2016-11-01 12:54:20 +0900

    update reset.php

--- reset.php
+++ reset.php
@@ -1,6 +1,10 @@
 <?php
 require("class.php");
 
+$request = isset($_SERVER['HTTP_X_REQUESTED_WITH']) ? strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) : "";
+if($request !== 'xmlhttprequest')
+	exit;
+
 if(isset($_POST['room'])){
 	$room = $_POST['room'];
 	$sql = "UPDATE desk SET status = 0 WHERE room = {$room}"
